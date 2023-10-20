c442c653dd2bbe3566a6d0847232c55cd74b0f5b
Eduard Beukes <edpbeukes@gmail.com>
2023-10-20 23:00:50 +0200

    Updated aspect and movement system to use the new aspect.

--- DOTS_ECS/Assets/Scripts/MoveToPositionAspect.cs
+++ DOTS_ECS/Assets/Scripts/MoveToPositionAspect.cs
@@ -1,12 +1,19 @@
 using Unity.Entities;
 using Unity.Transforms;
+using Unity.Mathematics;
 using UnityEngine;
 
 public readonly partial struct MoveToPositionAspect : IAspect
 {
     private readonly Entity _entity;//This is an optional field.
     
-    private readonly RefRO<LocalTransform> _transform;
+    private readonly RefRW<LocalTransform> _transform;
     private readonly RefRO<Speed> _speed;
-    private readonly RefRW<TargetPosition> _targetPosition;
+    private readonly RefRO<TargetPosition> _targetPosition;
+ 
+    public void Move(float deltaTime)
+    {
+        var direction = math.normalize(_targetPosition.ValueRO.Value - _transform.ValueRO.Position );
+        _transform.ValueRW.Position += direction * _speed.ValueRO.Value * deltaTime;
+    }
 }
--- DOTS_ECS/Assets/Scripts/MovingSystemBase.cs
+++ DOTS_ECS/Assets/Scripts/MovingSystemBase.cs
@@ -1,21 +1,13 @@
-using System;
 using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms;
-using UnityEngine;
 
 public partial class MovingSystemBase : SystemBase
 {
     protected override void OnUpdate()
     {
         Entities.ForEach(
-            (ref LocalTransform t, in Speed s, in TargetPosition p) =>
+            (MoveToPositionAspect moveTo) =>
             {
-                var direction = math.normalize(p.Value - t.Position);
-                t.Position += direction * s.Value * SystemAPI.Time.DeltaTime;
+                moveTo.Move(SystemAPI.Time.DeltaTime);
             }).Schedule();
-                //.Run(): Runs the code on the main thread.
-                //.Schedule(): Runs the code in a single worker thread.
-                //.ScheduleParallel(): Runs the code on multiple worker threads.
     }
 }
