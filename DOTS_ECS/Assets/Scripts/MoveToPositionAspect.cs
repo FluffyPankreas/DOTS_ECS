d44a3de01f0ee60685aa58c979ed9a5ed66352aa
Eduard Beukes <edpbeukes@gmail.com>
2023-10-20 22:40:51 +0200

    Added attributes to the move position aspect.

--- DOTS_ECS/Assets/Scripts/MoveToPositionAspect.cs
+++ DOTS_ECS/Assets/Scripts/MoveToPositionAspect.cs
@@ -1,6 +1,12 @@
 using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
 
 public readonly partial struct MoveToPositionAspect : IAspect
 {
-
+    private readonly Entity _entity;//This is an optional field.
+    
+    private readonly RefRO<LocalTransform> _transform;
+    private readonly RefRO<Speed> _speed;
+    private readonly RefRW<TargetPosition> _targetPosition;
 }
