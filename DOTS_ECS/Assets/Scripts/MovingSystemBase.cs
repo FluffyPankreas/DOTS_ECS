f7dec0b93c36c8da9d5a839f3efcd45a71235d64
Eduard Beukes <edpbeukes@gmail.com>
2023-10-20 19:45:21 +0200

    Added moving system base script to project.

---
+++ DOTS_ECS/Assets/Scripts/MovingSystemBase.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+public partial class MovingSystemBase : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
