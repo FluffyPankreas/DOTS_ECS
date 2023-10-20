ce5527cda86c96c5f11ceda9d3b5f4def1538fc4
Eduard Beukes <edpbeukes@gmail.com>
2023-10-20 20:17:57 +0200

    Implemented functionality to move the entities with speed components. Not working properly yet.

--- DOTS_ECS/Assets/Scripts/MovingSystemBase.cs
+++ DOTS_ECS/Assets/Scripts/MovingSystemBase.cs
@@ -1,9 +1,18 @@
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 public partial class MovingSystemBase : SystemBase
 {
     protected override void OnUpdate()
     {
-        throw new System.NotImplementedException();
+        Entities.ForEach(
+            (ref LocalTransform t, in Speed s) =>
+            {
+                t.Position += new float3(SystemAPI.Time.DeltaTime * s.Value, 0, 0);
+            }).Schedule();
+                //.Run(): Runs the code on the main thread.
+                //.Schedule(): Runs the code in a single worker thread.
+                //.ScheduleParallel(): Runs the code on multiple worker threads.
     }
 }
