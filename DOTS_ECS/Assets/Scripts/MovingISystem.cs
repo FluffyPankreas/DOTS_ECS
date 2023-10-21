903ef5d46fc8513b4a5ac904269eb28f78be2b12
Eduard Beukes <edpbeukes@gmail.com>
2023-10-21 10:52:26 +0200

    Duplicated functionionality from the system base movement system.

--- DOTS_ECS/Assets/Scripts/MovingISystem.cs
+++ DOTS_ECS/Assets/Scripts/MovingISystem.cs
@@ -2,4 +2,15 @@ using Unity.Entities;
 
 public partial struct MovingISystem : ISystem
 {
+    public void OnUpdate(ref SystemState state)
+    {
+        var randomGenerator = SystemAPI.GetSingleton<RandomComponent>();
+
+        foreach (var moveToPositionAspect in SystemAPI.Query<MoveToPositionAspect>())
+        {
+            moveToPositionAspect.Move(SystemAPI.Time.DeltaTime,randomGenerator);
+        }
+    }
+    
+    public struct MoveJob
 }
