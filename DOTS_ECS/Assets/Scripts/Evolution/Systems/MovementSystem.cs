23419a1ea830a758fcd77649502339a0473a4622
Eduard Beukes <edpbeukes@gmail.com>
2023-10-21 22:43:22 +0200

    Added outline for movement system to the project.

---
+++ DOTS_ECS/Assets/Scripts/Evolution/Systems/MovementSystem.cs
@@ -0,0 +1,12 @@
+using Unity.Entities;
+
+namespace Evolution.Systems
+{
+    public partial struct MovementSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            //TODO: Add the movement code here.
+        }
+    }
+}
