bd12ade30cb90ed5e5eaf453161e3bd977c4fda1
Eduard Beukes <edpbeukes@gmail.com>
2023-10-22 01:28:21 +0200

    Implemented move to destination aspect.

--- DOTS_ECS/Assets/Scripts/Evolution/Aspects/MoveToDestinationAspect.cs
+++ DOTS_ECS/Assets/Scripts/Evolution/Aspects/MoveToDestinationAspect.cs
@@ -1,9 +1,22 @@
+using Evolution.Components;
 using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
 
 namespace Evolution.Aspects
 {
     public readonly partial struct MoveToDestinationAspect : IAspect
     {
+        private readonly Entity _entity;
         
+        private readonly RefRW<LocalTransform> _localTransform;
+        private readonly RefRO<SpeedComponent> _speed;
+        private readonly RefRO<DestinationComponent> _destination;
+
+        public void Walk(float deltaTime)
+        {
+            var direction = math.normalize(_destination.ValueRO.Destination - _localTransform.ValueRO.Position);
+            _localTransform.ValueRW.Position += direction * _speed.ValueRO.WalkSpeed * deltaTime;
+        }
     }
 }
