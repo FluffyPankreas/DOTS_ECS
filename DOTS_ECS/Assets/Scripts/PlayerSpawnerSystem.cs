66e019a496138293f3c85f63034bd73dd0d892f4
Eduard Beukes <edpbeukes@gmail.com>
2023-10-21 14:12:03 +0200

    Implemented player spawning.

--- DOTS_ECS/Assets/Scripts/PlayerSpawnerSystem.cs
+++ DOTS_ECS/Assets/Scripts/PlayerSpawnerSystem.cs
@@ -2,8 +2,25 @@ using Unity.Entities;
 
 public partial class PlayerSpawnerSystem : SystemBase
 {
+    private int _spawnAmount = 20;
     protected override void OnUpdate()
     {
+        var entityQuery = EntityManager.CreateEntityQuery(typeof(PlayerTag));
+        var randomComponent = SystemAPI.GetSingletonRW<RandomComponent>();
+
+        var entityCommandBuffer = 
+            SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+            .CreateCommandBuffer(World.Unmanaged);
         
+        
+        var playerSpawnerComponent = SystemAPI.GetSingleton<PlayerSpawnerComponent>();
+        if (entityQuery.CalculateEntityCount() < _spawnAmount)
+        {
+            var spawnedEntity = entityCommandBuffer.Instantiate(playerSpawnerComponent.PlayerPrefab);
+            entityCommandBuffer.SetComponent(spawnedEntity, new Speed
+            {
+                Value = randomComponent.ValueRW.RandomGenerator.NextFloat(2f,5f)
+            });
+        }
     }
 }
