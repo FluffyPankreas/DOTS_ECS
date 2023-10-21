f070b2f13ed4786382052c1d6c413f4e9cfb8548
Eduard Beukes <edpbeukes@gmail.com>
2023-10-21 22:50:47 +0200

    Updated the movement system to schedule the move to destination job.

--- DOTS_ECS/Assets/Scripts/Evolution/Systems/MovementSystem.cs
+++ DOTS_ECS/Assets/Scripts/Evolution/Systems/MovementSystem.cs
@@ -1,12 +1,15 @@
+using Unity.Burst;
 using Unity.Entities;
+using Evolution.Jobs;
 
 namespace Evolution.Systems
 {
+    [BurstCompile]
     public partial struct MovementSystem : ISystem
     {
         public void OnUpdate(ref SystemState state)
         {
-            //TODO: Add the movement code here.
+            new MoveToDestinationJob().ScheduleParallel();
         }
     }
 }
