adfc672e368a98fc9ec35a66a2840780997a1722
Eduard Beukes <edpbeukes@gmail.com>
2023-10-22 09:18:58 +0200

    Added a function to find food to the herbivore aspect.

--- DOTS_ECS/Assets/Scripts/Evolution/Aspects/HerbivoreAspect.cs
+++ DOTS_ECS/Assets/Scripts/Evolution/Aspects/HerbivoreAspect.cs
@@ -7,5 +7,10 @@ namespace Evolution.Aspects
         private readonly Entity _entity;
         
         private readonly MoveToDestinationAspect _moveToDestinationAspect;
+
+        public void FindFood()
+        {
+            //TODO: Implement the code to set the destination with a found food source. 
+        }
     }
 }
