21b1c6f86f31235398a8392870ff0f11d47a3c81
Eduard Beukes <edpbeukes@gmail.com>
2023-10-22 09:03:14 +0200

    Added the herbivore agency system script to the project.

---
+++ DOTS_ECS/Assets/Scripts/Evolution/Systems/HerbivoreAgencySystem.cs
@@ -0,0 +1,13 @@
+using Unity.Burst;
+using Unity.Entities;
+
+namespace Evolution.Systems
+{
+    [BurstCompile]
+    public partial struct HerbivoreAgencySystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+        }
+    }
+}
